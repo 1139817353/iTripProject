18824b12cabf8cd9edd7604413342e86f4e4fb71
1139817353 <1139817353@qq.com>
2020-03-19 22:46:47 +0800

    自娱自乐

--- itrip-eureka-server/src/main/java/cn/ekgc/itrip/EurekaServerStarter.java
+++ itrip-eureka-server/src/main/java/cn/ekgc/itrip/EurekaServerStarter.java
@@ -6,11 +6,15 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
  * <b>爱旅行-注册中心启动类</b>
+ * @author Arthur
+ * @version 1.0.0
+ * @since 1.0.0
  */
 @EnableEurekaServer
 @SpringBootApplication
-public class ItripEurekaServerStarter {
+public class EurekaServerStarter {
 	public static void main(String[] args) {
-		SpringApplication.run(ItripEurekaServerStarter.class,args);
+    SpringApplication.run(EurekaServerStarter.class, args);
 	}
 }
+
